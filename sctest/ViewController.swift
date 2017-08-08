a22e3bc54c15de05a0041814874ba7cf78918b74
Gabor Pinter <pintergabor1990@gmail.com>
2017-08-08 11:41:13 +0200

    Initial Commit

---
+++ sctest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sctest
+//
+//  Created by Gabor Pinter on 2017. 08. 08..
+//  Copyright © 2017. Gabor Pinter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
